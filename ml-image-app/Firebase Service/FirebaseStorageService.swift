56fb0f59329fed14af9b6b476a8c852a4caebc33
levi davis <levidavis@pursuit.org>
2019-12-09 15:56:04 -0500

    Adds FirebaseStoreService

--- ml-image-app/Firebase Service/FirebaseStorageService.swift
+++ ml-image-app/Firebase Service/FirebaseStorageService.swift
@@ -14,38 +14,29 @@ class FirebaseStorageService {
     
     private let storage: Storage!
     private let storageReference: StorageReference
+    private let imagesFolderReference: StorageReference
+    
+    private init() {
+        storage = Storage.storage()
+        storageReference = storage.reference()
+        imagesFolderReference = storageReference.child("images")
+    }
+    
+    func storeImage(image: Data, completion: @escaping (Result<URL, Error>) -> ()) {
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        let uuid = UUID()
+        let imageLocation = imagesFolderReference.child(uuid.description)
+        imageLocation.putData(image, metadata: metadata) { (responseMetadata, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                imageLocation.downloadURL { (url, error) in
+                    guard error == nil, let url = url else {completion(.failure(error!)); return}
+                    completion(.success(url))
+                }
+            }
+        }
+    }
+    
 }
-
-/**
- class FirebaseStorageService {
-     
-     static let manager = FirebaseStorageService()
-     
-     private let storage: Storage!
-     private let storageReference: StorageReference
-     private let imagesFolderReference: StorageReference
-     
-     private init() {
-         storage = Storage.storage()
-         storageReference = storage.reference()
-         imagesFolderReference = storageReference.child("images")
-     }
-     
-     func storeImage(image: Data, completion: @escaping (Result <URL, Error>) -> ()) {
-         let metaData = StorageMetadata()
-         metaData.contentType = "image/jpeg"
-         let uuid = UUID()
-         let imageLocation = imagesFolderReference.child(uuid.description)
-         imageLocation.putData(image, metadata: metaData) { (responseMetadata, error) in
-             if let error = error {
-                 completion(.failure(error))
-             } else {
-                 imageLocation.downloadURL { (url, error) in
-                     guard error == nil, let url = url else {completion(.failure(error!)); return}
-                     completion(.success(url))
-                 }
-             }
-         }
-     }
- }
- */
