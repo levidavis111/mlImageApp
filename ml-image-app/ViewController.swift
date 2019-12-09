e0aaf62a270bb2eb638dd46d582e633e68c2dcc8
levi davis <levidavis@pursuit.org>
2019-12-09 14:50:59 -0500

    First commit

---
+++ ml-image-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ml-image-app
+//
+//  Created by Levi Davis on 12/9/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ml-image-appTests/ml_image_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  ml_image_appTests.swift
+//  ml-image-appTests
+//
+//  Created by Levi Davis on 12/9/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import XCTest
+@testable import ml_image_app
+
+class ml_image_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
